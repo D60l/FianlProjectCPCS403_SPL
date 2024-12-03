4b232bc5eb42ca6ebf7c570dca3626ca1aafe02a
MH Alyamani <87152986+D60l@users.noreply.github.com>
2024-12-04 00:57:59 +0300

    Add files via upload

--- Pages/AlIttihadGallery.php
+++ Pages/AlIttihadGallery.php
@@ -1,6 +1,8 @@
 <?php
-include('../Includes/session_manager.php');
+// Include the script to fetch gallery data
+include('../Includes/gallery.php');
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -13,10 +15,9 @@ include('../Includes/session_manager.php');
 
 <body>
     <div>
-        <?php
-        include("../Includes/Header.php");
-        ?>
+        <?php include("../Includes/Header.php"); ?>
     </div>
+
     <section class="image-gallery">
         <h2>Al-Ittihad Gallery</h2>
         <div class="preview-section">
@@ -25,55 +26,29 @@ include('../Includes/session_manager.php');
             </div>
             <div class="preview-text">
                 <h3 id="main-title">Al-Ittihad FC</h3>
-                <p id="main-description">Al Ittihad is one of the oldest and most successful football clubs in Saudi
-                    Arabia, based in Jeddah.The team
-                    plays its home matches at the King Abdullah Sports City stadium, and there is more, to go to the website of Al-Ittihad <a href="https://ittihadclub.sa/">Click
-                    here.</a></p>
+                <p id="main-description">Al Ittihad is one of the oldest and most successful football clubs in Saudi Arabia, based in Jeddah. 
+                The team plays its home matches at the King Abdullah Sports City stadium.</p>
             </div>
         </div>
-        <!-- Horizontal line between thunbnails and text -->
         <hr>
-        <!-- Gallery container -->
         <div class="gallery-container">
             <div class="gallery">
-                <img src="../Images/Teams/Al-Ittihad-FC.png" 
-                    alt="Thumbnail 1"
-                    data-title="Al-Ittihad FC"
-                    data-description="Al Ittihad is one of the oldest and most successful football clubs in Saudi Arabia, based in Jeddah. 
-                    The team plays its home matches at the King Abdullah Sports City stadium." 
-                    onclick="showMedia(this)">
-                <img src="../Images/AlittihadHome.png" 
-                    alt="Thumbnail 2" 
-                    data-title="Club's Headquarters"
-                    data-description="This is Headquarters of Al-Ittihad Club in Jeddah on Al-Sahafh Street"
-                    onclick="showMedia(this)">
-                <img src="../Images/AlittihadFans.jpg" 
-                    alt="Thumbnail 3" 
-                    data-title="Al-Ittihad Fans"
-                    data-description="Al-Ittihad fans are the biggest fanbase in Saudi Arabia. -
-                    Their statistics show they are far ahead of competitors in attendance by nearly 1,100,000 fans, and their attendance in recent years has reached more than 3,600,000."
-                    onclick="showMedia(this)">
-                <img src="../Images/AboNoran2.jpg" 
-                    alt="Thumbnail 4" 
-                    data-title="Club Legend"
-                    data-description="Al-Ittihad midfielder Mohamed Noor, the legend of Al-Ittihad Club, joined his club in 1993. 
-                    Noor played with Al-Ittihad for 23 years and won 20 cups." 
-                    onclick="showMedia(this)">
-                <!-- Video Thumbnail -->
-                <img src="../Images/VideoThumbnail.png"
-                    alt="Thumbnail 5" 
-                    data-title="Crowning Alittihad"
-                    data-description="In  2023/6/1 Al-Ittihad became the champion of Saudi League 'SPL', It was unforgettable moment."
-                    data-video="../Videos/CrowningAlittihad2.mp4"
-                    onclick="showMedia(this)">
+                <?php foreach ($galleryItems as $item): ?>
+                    <img src="<?php echo htmlspecialchars($item['image_path']); ?>"
+                        alt="<?php echo htmlspecialchars($item['title']); ?>"
+                        data-title="<?php echo htmlspecialchars($item['title']); ?>"
+                        data-description="<?php echo htmlspecialchars($item['description']); ?>"
+                        <?php if (!empty($item['video_path'])): ?>
+                            data-video="<?php echo htmlspecialchars($item['video_path']); ?>"
+                        <?php endif; ?>
+                        onclick="showMedia(this)">
+                <?php endforeach; ?>
             </div>
-
         </div>
     </section>
+
     <div>
-        <?php
-        include("../Includes/Footer.php");
-        ?>
+        <?php include("../Includes/Footer.php"); ?>
     </div>
 
     <script src="../javaScript/gallery.js"></script>
